52419d1b05f235dba445833131fd1f00c0d25677
soyoung47 <0407sally@naver.com>
2020-07-27 00:19:41 +0900

    Upload Codility L10-1

--- Codility/28. L10-1_Count Factors.cpp
+++ Codility/28. L10-1_Count Factors.cpp
@@ -1,5 +1,22 @@
 /*
-    Time Complexity: 
-    Result:::
-    Reference:::
+    Time Complexity: O(sqrt(N))
+    Result:::https://app.codility.com/demo/results/trainingPQNDRZ-RAV/
 */
+#include <math.h>
+
+int solution(int N) {
+
+    int cnt = 0;
+
+    for (int i = 1; i <= sqrt(N); i++)
+    {
+        if (N % i == 0)
+        {
+            cnt += 2;
+            if (N / i == i)
+                cnt--;
+        }
+    }
+
+    return cnt;
+}
