a8fcb4ee938be808d5e86725d0c924304ba87088
soyoung47 <0407sally@naver.com>
2020-08-09 20:21:19 +0900

    Update Codility L11-1 (100%)

--- Codility/32. L11-1_Count Non Divisible.cpp
+++ Codility/32. L11-1_Count Non Divisible.cpp
@@ -1,46 +1,40 @@
-//77% : https://app.codility.com/demo/results/trainingN9ANYU-FH8/
-
-#include <unordered_map>
+/*
+    Time Complexity: O(N * log(N))
+    Result::: https://app.codility.com/demo/results/trainingTBHMCP-HNE/
+    Reference::: http://dal4segno.github.io/algorithm/codility-11-sieve-of-eratosthenes/
+*/
 #include <algorithm>
 
 vector<int> solution(vector<int>& A) {
 
     int size = A.size();
     vector<int> ans;
-    unordered_map<int, int> m, num;
+    vector<int> num(size * 2 + 1, 0);
 
-    //Sort array v & Count elements
-    vector<int> v = A;
-    sort(v.begin(), v.end());
-        
+    //Count elements
     for (int i = 0; i < size; i++)
     {
-        num[v[i]]++;
+        num[A[i]]++;
     }
-
-    int min_cnt = num[v[0]];
-    m[v[0]] = size - min_cnt;
 
     //Count Non Divisors
-    for (int i = 1; i < size; i++)
+    for (int i = 0; i < size; ++i)
     {
-        int elemt = v[i];
-        if (m.find(elemt) != m.end())   continue;
-        
-        int cnt = 0;
-        for (int j = 0; j < i; j++)
+        int elemt = A[i];
+        int divisor = 0;
+        for (int j = 1; j * j <= elemt; ++j)
         {
-            if (elemt % v[j] != 0)
-                cnt++;
+            if (elemt % j == 0)
+            {
+                divisor += num[j];
+                if (elemt / j != j)
+                {
+                    divisor += num[elemt / j];
+                }
+            }
         }
-        
-        m[v[i]] = cnt + size - i - num[v[i]];
-    }
 
-    //Push answers
-    for (int i = 0; i < size; i++)
-    {
-        ans.push_back(m[A[i]]);
+        ans.push_back(size - divisor);
     }
 
     return ans;
