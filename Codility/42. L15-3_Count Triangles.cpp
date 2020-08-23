843431249be7c77e5c48c79a91fa3cbfc7995f12
soyoung47 <sy47lee@gmail.com>
2020-08-24 02:28:25 +0900

    Upload Codility L15-3 (100%)

--- Codility/42. L15-3_Count Triangles.cpp
+++ Codility/42. L15-3_Count Triangles.cpp
@@ -1,6 +1,7 @@
 /*
     Time Complexity: O(N**2)
-    Result::: https://app.codility.com/demo/results/trainingK2YYFV-5JD/
+    Result::: https://app.codility.com/demo/results/trainingX7K5QM-ADC/
+    Reference::: https://app.codility.com/demo/results/trainingMJ4X6H-P9M/
 */
 
 #include <algorithm>
@@ -11,6 +12,8 @@ int solution(vector<int>& A)
     if (N < 3)   return 0;
 
     sort(A.begin(), A.end());
+
+    if (A[0] + A[1] > A[N - 1])    return N * (N - 1) * (N - 2) / 6;
 
     int cnt = 0;
     for (int a = 0; a < N - 2; a++)
