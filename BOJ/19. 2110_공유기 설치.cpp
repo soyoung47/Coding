5a0d121bb07f94e773412856ff4980083d5cb7eb
soyoung47 <sy47lee@gmail.com>
2020-12-22 21:55:54 +0900

    Modify BOJ 2110_공유기 설치

--- BOJ/19. 2110_공유기 설치.cpp
+++ BOJ/19. 2110_공유기 설치.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 int main()
 {
-	int N, C;
+	int N, C;	//N: 집의 개수, C: 공유기 개수
 	cin >> N >> C;
 
-	vector<long long> house(N, 0);
+	vector<long long> house(N, 0);	///집 위치 배열
 	for (int i = 0; i < N; i++)
 		cin >> house[i];
 
@@ -23,6 +23,24 @@ int main()
 		mid = (start + end) / 2;
 
 		//공유기 설치
+		int cnt = 1;
+		long long prev = house[0];
+		for (int i = 1; i < house.size(); i++)
+		{
+			if (house[i] - prev >= mid)
+			{
+				cnt++;
+				prev = house[i];
+			}
+		}
+
+		if (cnt >= C)
+		{
+			start = mid + 1;
+			max = max > mid ? max : mid;
+		}	
+		else
+			end = mid - 1;
 	}
 
 	cout << max;
