7acfb9ab9dced83b463160570864791ce6ced100
soyoung47 <sy47lee@gmail.com>
2021-02-24 00:18:11 +0900

    Upload BOJ 2248_이진수 찾기

--- BOJ/56. 2248_이진수 찾기.cpp
+++ BOJ/56. 2248_이진수 찾기.cpp
@@ -1,51 +1,55 @@
 //https://www.acmicpc.net/problem/2248
-//
+//2016KB, 0ms
 
-
-//dp[][] 이차배열 사용
-//메모리 초과
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-int countOne(int num)
+string ans = "";
+vector<vector<int>> v;
+
+void findBinary(int n, int l, long long ind)
 {
-	int cnt = 0;
-	while (num != 0)
+	if (n == 0)	return;
+	if (l == 0)
 	{
-		num &= num - 1;
-		cnt++;
+		for (int i = 0; i < n; i++)
+			ans += '0';
+		return;
 	}
 
-	return cnt;
+	long long cnt = v[l][n - 1];
+
+	if (cnt >= ind)
+	{
+		ans += '0';
+		findBinary(n - 1, l, ind);
+	}
+	else
+	{
+		ans += '1';
+		findBinary(n - 1, l - 1, ind - cnt);
+	}
+
+	return;
 }
 
 int main()
 {
-	int n, l, ind;
+	int n, l;
+	long long ind;
 	cin >> n >> l >> ind;
 
-	vector<int> v;
+	//1의 개수: v[i][j] (i = i개 이하의 1 개수, j = 자릿수)
+	v.assign(l + 1, vector<int>(n + 1, 1));
 
-	int num = 0;
-	while (1)
+	for (int i = 1; i <= l; i++)
 	{
-		if (v.size() == ind)	break;
-
-		if (countOne(num) <= l)
-			v.emplace_back(num);
-
-		num += 1;
+		for (int j = 1; j <= n; j++)
+			v[i][j] = v[i - 1][j - 1] + v[i][j - 1];	//앞에 '1' 붙이는 경우 + '0' 붙이는 경우
 	}
 
-	//이진수로 변환
-	int tmp = v.back();
-	string ans = to_string(tmp % 2);
-	while (tmp /= 2)
-	{
-		ans = to_string(tmp % 2) + ans;
-	}
+	findBinary(n, l, ind);
 
 	cout << ans;
 }
@@ -62,70 +66,4 @@ int main()
 //	}
 //
 //	return cnt;
-//}
-
-
-//0		0
-//1		1
-//--------
-//10	1	
-//11	2
-//--------
-//100	1
-//101	2
-//110	2
-//111	3
-//--------
-//1000	1
-//1001	2
-//1010	2
-//1011	3
-//1100	2
-//1101	3	
-//1110	3
-//1111	4
-
-////메모리 초과
-//
-//#include <iostream>
-//#include <vector>
-//#include <math.h>
-//#include <string>
-//using namespace std;
-//
-//int main()
-//{
-//	int n, l, ind;
-//	cin >> n >> l >> ind;
-//
-//	vector<int> one = { 0,1 };
-//	vector<int> v = { 0, 1 };
-//
-//	for (int i = 1; i < n; i++)
-//	{
-//		for (int j = 0; j < pow(2, i); j++)
-//		{
-//			int cnt = one[j] + 1;
-//			one.emplace_back(cnt);
-//			if (cnt <= l)
-//			{
-//				v.emplace_back(pow(2, i) + j);
-//				if (v.size() == ind)
-//				{
-//					i = n + 1;
-//					break;
-//				}
-//			}
-//		}
-//	}
-//
-//	//이진수로 변환
-//	int tmp = v[ind];
-//	string ans = to_string(tmp % 2);
-//	while (tmp /= 2)
-//	{
-//		ans = to_string(tmp % 2) + ans;
-//	}
-//
-//	cout << ans;
 //}
